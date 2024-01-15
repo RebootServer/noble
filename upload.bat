b63adb37f4291343c3c67742e4329afd3fc97cf2
MapleStoryReboot <rlaalsrldl@gmail.com>
2024-01-16 03:01:15 +0900

    而ㅻ컠 硫붿떆吏瑜??낅젰?섏꽭??

--- upload.bat
+++ upload.bat
@@ -2,9 +2,16 @@
 
 cd C:\Users\rlaal\OneDrive\바탕 화면\테스트\Noble
 
-REM GitHub로 푸시
+REM 변경 사항 스테이징
 git add .
-git commit -m "winter"
+
+REM 커밋
+git commit -m "커밋 메시지를 입력하세요."
+
+REM 원격 저장소에서 변경사항 가져오기
+git pull origin main
+
+REM 변경 사항 다시 푸시
 git push origin main
 
 echo "업로드 완료"
